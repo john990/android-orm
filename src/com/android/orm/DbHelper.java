7dfa93eccaf0da06619d9bef2b6c9eeb8cd4cc99
kai.wang <kai.wang.john@gmail.com>
2014-01-13 15:00:02 +0800

    helper ok ready to test

---
+++ src/com/android/orm/DbHelper.java
@@ -0,0 +1,53 @@
+package com.android.orm;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by kai.wang on 1/13/14.
+ */
+public class DbHelper extends SQLiteOpenHelper{
+
+	private static int DB_VERSION = 1;
+
+	private static String DATABASE_PATH = null;
+	private static String DATABASE_FILENAME = null;
+
+	public DbHelper(Context context) {
+		super(context, DATABASE_PATH + DATABASE_FILENAME, null, DB_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+	}
+
+	/**
+	 * 设置数据库版本
+	 * @param version
+	 */
+	public static void setDbVersion(int version){
+		DbHelper.DB_VERSION = version;
+	}
+
+	/**
+	 * 设置数据库路径
+	 * @param path
+	 */
+	public static void setDatabasePath(String path) {
+		DbHelper.DATABASE_PATH = path;
+	}
+
+	/**
+	 * 设置数据库名
+	 * @param filename
+	 */
+	public static void setDatabaseFilename(String filename) {
+		DbHelper.DATABASE_FILENAME = filename;
+	}
+}
---
+++ src/com/android/orm/QueryHelper.java
@@ -0,0 +1,91 @@
+package com.android.orm;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by kai.wang on 1/13/14.
+ */
+public class QueryHelper {
+	private static ExecutorService executor;
+	private static SQLiteDatabase db;
+
+	static {
+		executor = Executors.newSingleThreadExecutor();
+	}
+
+	public static void init(SQLiteOpenHelper helper) {
+		db = helper.getWritableDatabase();
+	}
+
+	public interface UpdateCallBack {
+		void onFinish();
+	}
+
+	public interface FindBeanCallBack<T> {
+		<T> void onFinish(T bean);
+	}
+
+	public interface FindBeansCallBack<T> {
+		<T> void onFinish(List<T> bean);
+	}
+
+	/**
+	 * 更新数据库（update,insert,delete）
+	 *
+	 * @param sql
+	 * @param params
+	 * @param callBack
+	 */
+	public static void update(final String sql, final String[] params, final UpdateCallBack callBack) {
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				db.execSQL(sql, params);
+				if (callBack != null) callBack.onFinish();
+			}
+		});
+	}
+
+
+	/**
+	 * 查找单个bean
+	 * @param cls
+	 * @param sql
+	 * @param params
+	 * @param callBack
+	 * @param <T>
+	 */
+	public static <T> void findBean(final Class<T> cls, final String sql, final String[] params, final FindBeansCallBack<T> callBack) {
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				Cursor cursor = db.rawQuery(sql, params);
+				if (callBack != null) callBack.onFinish(BeanUtil.cursorToBeans(cursor, cls));
+			}
+		});
+	}
+
+	/**
+	 * 查找多个bean
+	 * @param cls
+	 * @param sql
+	 * @param params
+	 * @param callBack
+	 * @param <T>
+	 */
+	public static <T> void findBeans(final Class<T> cls, final String sql, final String[] params, final FindBeanCallBack<T> callBack) {
+		executor.execute(new Runnable() {
+			@Override
+			public void run() {
+				Cursor cursor = db.rawQuery(sql, params);
+				if (callBack != null) callBack.onFinish(BeanUtil.cursorToBean(cursor, cls));
+			}
+		});
+	}
+}
